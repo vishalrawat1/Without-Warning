ec396f099d13f6f90c8d4c52888a2f7c0045abc0
Harshit <bharshit468@gmail.com>
2024-04-16 13:13:19 +0530

    Redux funtionality Done

--- Backend/index.js
+++ Backend/index.js
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 // import { AdminModel } from "./Models/Adminmodel";
 import adminRoute from "./Routes/Adminroutes.js";
-const app = express(); 
+const app = express();
 
 dotenv.config();
+import cors from "cors";
 
-const PORT = process.env.PORT || 3000
-const MONGOURL = process.env.MONGO_URL
-console.log(PORT)
+const PORT = process.env.PORT || 3000;
+const MONGOURL = process.env.MONGO_URL;
+console.log(PORT);
 
 // app.use(
 //   cors({
@@ -18,24 +19,33 @@ console.log(PORT)
 //     allowedHeaders: ['Content-Type'],
 //   })
 // )
-//-- post resq 
+//-- post resq
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use('/admin' , adminRoute)
-//-- 
-app.get('/' , (req ,res)=>{
-    console.log(req);
-    res.status(234).send("WELCOME");
-})
-
-mongoose.connect(MONGOURL).then(()=>{
-console.log("app connected to db");
-app.listen(PORT, (req , res) => {
-  console.log("Server is running on port " + PORT);
+app.use("/admin", adminRoute);
+//--
+app.get("/", (req, res) => {
+  console.log(req);
+  res.status(234).send("WELCOME");
 });
-}).catch((error)=>{
-console.log(error);
-})
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("app connected to db");
+    app.listen(PORT, (req, res) => {
+      console.log("Server is running on port " + PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // everything ok
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,62 +1,10 @@
-import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
+import React from "react";
 
 const Home = () => {
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//     setLoading(true);
-//     axios.get('https://bookstore2-d9fv.onrender.com/books')
-//       .then((res) => {
-//         setBooks(res.data.data);
-//         setLoading(false);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//         setLoading(false);
-//       });
-//   }, []);
-
   return (
-    <div className="d-flex justify-content-center align-items-center flex-column">
-      <div className='text-dark' style={{ paddingLeft: '0%' }}>
-        DATA
-        <hr></hr>
-      </div>
-      <table className='border-separate border-spacing-2'>
-        <thead>
-          <tr>
-            <th className='border border-slate-600 rounded-md'>Sno</th>
-            <th className='border border-slate-600 rounded-md'>Name</th>
-            <th className='border border-slate-600 rounded-md'>Email</th>
-            <th className='border border-slate-600 rounded-md'>Risk Score</th>
-            <th className='border border-slate-600 rounded-md'>Prompt</th>
-          </tr>
-        </thead>
-        <tbody>
-            <tr className='h-8'>
-              <td className='border border-slate-600 rounded-md text-center'>
-                {/* {index + 1} */} 
-              </td>
-              <td className='border border-slate-600 rounded-md text-center'>
-                {/* {book.title} */}
-              </td>
-              <td className='border border-slate-600 rounded-md text-center'>
-                {/* {book.author} */}
-              </td>
-              <td className='border border-slate-600 rounded-md text-center'>
-                {/* {book.publishyear} */}
-              </td>
-              <td className='border border-slate-600 rounded-md text-center'>
-                {/* {book.publishyear} */}
-              </td>
-              <td className='border border-slate-600 rounded-md text-center'>
-                {/* {book.publishyear} */}
-              </td>
-            </tr>
-        </tbody>
-      </table>
+    <div>
+      <textarea placeholder="Please Enter your Text "></textarea>
+      <button>Submit</button>
     </div>
   );
 };
